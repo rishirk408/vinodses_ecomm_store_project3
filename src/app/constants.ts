8f3e65e9a0bedf3ed29c99571e0632cc8ab52927
rishirk408 <90163346+rishirk408@users.noreply.github.com>
2026-01-16 17:20:41 +0530

    Update BACKEND_API_URL to use HTTP protocol

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'https://a41bf42936e554344ab8c0d85622c64b-645968770.ap-south-1.elb.amazonaws.com/',
+    BACKEND_API_URL: 'http://a41bf42936e554344ab8c0d85622c64b-645968770.ap-south-1.elb.amazonaws.com',
 };
