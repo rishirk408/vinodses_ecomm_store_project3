009a383b07dc977d30a0cad07066d94582afde29
rishirk408 <90163346+rishirk408@users.noreply.github.com>
2026-01-16 20:37:21 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a41bf42936e554345622c64b-645968770.ap-south-1.elb.amazonaws.com/api',
+BACKEND_API_URL: 'http://a41bf42936e554344ab8c0d85622c64b-645968770.ap-south-1.elb.amazonaws.com'
 };
