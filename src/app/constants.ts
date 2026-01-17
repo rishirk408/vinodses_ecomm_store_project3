7cc1408fd6be1033b17025e99c19972d6f79158a
rishirk408 <90163346+rishirk408@users.noreply.github.com>
2026-01-17 19:32:34 +0530

    Update BACKEND_API_URL in constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-BACKEND_API_URL: 'http://a41bf42936e554344ab8c0d85622c64b-645968770.ap-south-1.elb.amazonaws.com',
+BACKEND_API_URL: 'http://a3d472a91eae74c3a9263eb1a3d2c68d-542099929.ap-south-1.elb.amazonaws.com',
 };
